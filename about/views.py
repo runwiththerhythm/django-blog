21bb34b7e3eed28bd790f0156b8473ab5004ceb8
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-07-23 20:05:29 +0100

    Set up test for forms

--- about/views.py
+++ about/views.py
@@ -5,7 +5,7 @@ from .forms import CollaborateForm
 
 # Create your views here.
 def about_me(request):
-       """
+    """
     Renders the most recent information on the website author
     and allows user collaboration requests.
 
@@ -14,20 +14,27 @@ def about_me(request):
     **Context**
     ``about``
         The most recent instance of :model:`about.About`.
-        ``collaborate_form``
-            An instance of :form:`about.CollaborateForm`.
-    
+    ``collaborate_form``
+        An instance of :form:`about.CollaborateForm`.
+
     **Template**
     :template:`about/about.html`
     """
+    # Get the most recent About object
+    about = About.objects.latest("updated_on")
+
+    # Handle POST request for collaboration
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
-            
-    about = About.objects.all().order_by('-updated_on').first()
-    collaborate_form = CollaborateForm()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+    else:
+        collaborate_form = CollaborateForm()
 
     return render(
         request,
