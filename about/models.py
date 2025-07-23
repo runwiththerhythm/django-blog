c388664b1fb7913fc13fdbe04328a0e329bb4d42
runwiththerhythm <0h4wb7n9@students.codeinstitute.net>
2025-07-23 19:42:29 +0100

    Add docstrings

--- about/models.py
+++ about/models.py
@@ -3,6 +3,9 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class About(models.Model):
+    """
+    Stores a single about me text.
+    """
     title = models.CharField(max_length=200, unique=True)
     profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
@@ -13,6 +16,9 @@ class About(models.Model):
         return self.title
 
 class CollaborateRequest(models.Model):
+    """
+    Stores a single collaboration request.
+    """
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
